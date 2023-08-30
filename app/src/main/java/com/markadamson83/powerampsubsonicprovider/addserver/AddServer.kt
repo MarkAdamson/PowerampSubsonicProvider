ab5d15fb77fbe735f86a877043d965cc99edf5f8
Mark Adamson <markadamson83@gmail.com>
2023-08-30 09:45:34 +0100

    Add Server field interactions complete

--- app/src/main/java/com/markadamson83/powerampsubsonicprovider/addserver/AddServer.kt
+++ app/src/main/java/com/markadamson83/powerampsubsonicprovider/addserver/AddServer.kt
@@ -1,5 +1,6 @@
 package com.markadamson83.powerampsubsonicprovider.addserver
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,18 +11,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.markadamson83.powerampsubsonicprovider.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(device = Devices.PIXEL_4_XL)
 fun AddServer() {
@@ -30,39 +39,22 @@ fun AddServer() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(R.string.add_a_server),
-                style = typography.titleLarge
-            )
-        }
+        ScreenTitle(R.string.add_a_server)
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(R.string.base_url_hint))
-            },
-            onValueChange = { }
+        var baseURL by remember { mutableStateOf("") }
+        var username by remember { mutableStateOf("")}
+        var password by remember { mutableStateOf("") }
+        BaseURLField(
+            value = baseURL,
+            onValueChange = { baseURL = it }
         )
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(R.string.username_hint))
-            },
-            onValueChange = { }
+        UsernameField(
+            value = username,
+            onValueChange = { username = it }
         )
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(R.string.password_hint))
-            },
-            onValueChange = { }
+        PasswordField(
+            value = password,
+            onValueChange = { password = it }
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
@@ -72,4 +64,75 @@ fun AddServer() {
             Text(text = stringResource(R.string.add_server))
         }
     }
+}
+
+@Composable
+private fun ScreenTitle(@StringRes resource: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(resource),
+            style = typography.titleLarge
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun BaseURLField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        label = {
+            Text(text = stringResource(R.string.base_url_hint))
+        },
+        onValueChange = onValueChange
+    )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun UsernameField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        label = {
+            Text(text = stringResource(R.string.username_hint))
+        },
+        onValueChange = onValueChange
+    )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun PasswordField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    var isVisible by remember { mutableStateOf(false) }
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        trailingIcon = {
+            IconButton(onClick = { isVisible = !isVisible }) {
+                Icon(
+                    painter = painterResource(id = if(isVisible) R.drawable.ic_visibility_off_24 else R.drawable.ic_visibility),
+                    contentDescription = stringResource(R.string.toggle_password_visibility)
+                )
+            }
+        },
+        visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        label = {
+            Text(text = stringResource(R.string.password_hint))
+        },
+        onValueChange = onValueChange
+    )
 }
