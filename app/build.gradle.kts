ee8ab731b02f5fdb9a6ff40f6b738d65a64799cd
Mark Adamson <markadamson83@gmail.com>
2023-08-30 11:29:01 +0100

    Stup for JUnit5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -89,5 +88,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-simplexml:2.9.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
