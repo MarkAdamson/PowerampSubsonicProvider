e6714b3efcc4c605741dde19fbd9029c0e1d492a
Mark Adamson <markadamson83@gmail.com>
2023-08-31 14:20:34 +0100

    Introduced custom application class

---
+++ app/src/main/java/com/markadamson83/powerampsubsonicprovider/app/PSPApplication.kt
@@ -0,0 +1,9 @@
+package com.markadamson83.powerampsubsonicprovider.app
+
+import android.app.Application
+
+class PSPApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
