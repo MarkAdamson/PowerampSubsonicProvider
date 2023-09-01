65f17960702c934b65896ad0a9bbc11c9e4364ed
Mark Adamson <markadamson83@gmail.com>
2023-09-01 11:01:09 +0100

    Fix UI tests

--- app/src/main/java/com/markadamson83/powerampsubsonicprovider/app/ApplicationModule.kt
+++ app/src/main/java/com/markadamson83/powerampsubsonicprovider/app/ApplicationModule.kt
@@ -3,12 +3,13 @@ package com.markadamson83.powerampsubsonicprovider.app
 import com.markadamson83.powerampsubsonicprovider.addserver.AddServerViewModel
 import com.markadamson83.powerampsubsonicprovider.domain.server.InMemoryServerStore
 import com.markadamson83.powerampsubsonicprovider.domain.server.ServerRepository
+import com.markadamson83.powerampsubsonicprovider.domain.server.ServerStore
 import com.markadamson83.powerampsubsonicprovider.domain.validation.ServerValidator
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val applicationModule = module {
-    single { InMemoryServerStore() }
+    single<ServerStore> { InMemoryServerStore() }
     factory { ServerValidator() }
     factory { ServerRepository(serverStore = get())}
 
