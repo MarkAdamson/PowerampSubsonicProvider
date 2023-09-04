ef9c3e0e002645a5331dd3d468ccc675928e3285
Mark Adamson <markadamson83@gmail.com>
2023-09-04 16:05:21 +0100

    Can detect when there are servers configured

--- app/src/main/java/com/markadamson83/powerampsubsonicprovider/servers/ServersViewModel.kt
+++ app/src/main/java/com/markadamson83/powerampsubsonicprovider/servers/ServersViewModel.kt
@@ -2,14 +2,17 @@ package com.markadamson83.powerampsubsonicprovider.servers
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.markadamson83.powerampsubsonicprovider.domain.server.Server
 import com.markadamson83.powerampsubsonicprovider.servers.state.ServersState
 
-class ServersViewModel {
+class ServersViewModel (
+    private val servers: List<Server> = emptyList()
+) {
 
     private val mutableServersState = MutableLiveData<ServersState>()
     val serversState: LiveData<ServersState> = mutableServersState
 
     fun getServers() {
-        mutableServersState.value = ServersState.Servers(emptyList())
+        mutableServersState.value = ServersState.Servers(servers)
     }
 }
--- app/src/test/java/com/markadamson83/powerampsubsonicprovider/servers/ServersTest.kt
+++ app/src/test/java/com/markadamson83/powerampsubsonicprovider/servers/ServersTest.kt
@@ -1,6 +1,7 @@
 package com.markadamson83.powerampsubsonicprovider.servers
 
 import com.markadamson83.powerampsubsonicprovider.InstantTaskExecutorExtension
+import com.markadamson83.powerampsubsonicprovider.domain.server.Server
 import com.markadamson83.powerampsubsonicprovider.servers.state.ServersState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -16,5 +17,16 @@ class ServersTest {
         viewModel.getServers()
 
         assertEquals(ServersState.Servers(emptyList()), viewModel.serversState.value)
+    }
+
+    @Test
+    fun serversConfigured() {
+        val demoServer = Server("DemoServerId", "Demo Server", "http://demo.subsonic.org", "guest1", "guest")
+        val demoServer2 = Server("DemoServer2Id", "Demo Server 2", "http://demo.subsonic.org", "guest2", "guest")
+        val viewModel = ServersViewModel(listOf(demoServer, demoServer2))
+
+        viewModel.getServers()
+
+        assertEquals(ServersState.Servers(listOf(demoServer, demoServer2)), viewModel.serversState.value)
     }
 }
